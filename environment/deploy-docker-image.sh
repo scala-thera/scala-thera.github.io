06e60ee9b0cd26574ee6bd0283579469ec79482b
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-11-03 15:10:45 +0100

    Update CI

--- environment/deploy-docker-image.sh
+++ environment/deploy-docker-image.sh
@@ -3,6 +3,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker build --no-cache -t \
   --file "$SCRIPT_DIR/Dockerfile" \
-  thera/thera-blog.com:$(date +%F) .
+  gondolav/thera-blog:$(date +%F) .
 docker login
-docker push thera/thera-blog.com:$(date +%F)
+docker tag 440f15e3e6a3 gondolav/thera-blog:$(date +%F)
+docker push gondolav/thera-blog:$(date +%F)
