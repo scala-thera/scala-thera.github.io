b3b8bc36c0f67faf0fbc85da563b32563e74b9d6
Gondolav <Gondolav@users.noreply.github.com>
2020-11-03 14:30:18 +0000

    deploy: 72df0b400a04204e35b797635d55b471c59198df

--- environment/deploy-docker-image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-docker build --no-cache -t \
-  --file "$SCRIPT_DIR/Dockerfile" \
-  gondolav/thera-blog:$(date +%F) .
-docker login
-docker tag 440f15e3e6a3 gondolav/thera-blog:$(date +%F)
-docker push gondolav/thera-blog:$(date +%F)
