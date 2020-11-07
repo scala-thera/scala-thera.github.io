6ac5064e9f903f3dbf2d0c7096afb3f6785c7238
Andrea Veneziano <andrea.veneziano@epfl.ch>
2020-11-07 15:05:04 +0100

    Fix tag problem in deploy-docker-image.sh

--- environment/deploy-docker-image.sh
+++ environment/deploy-docker-image.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build --no-cache -t \
+docker build --no-cache \
   --file "$SCRIPT_DIR/Dockerfile" \
-  gondolav/thera-blog:$(date +%F) .
+  -t gondolav/thera-blog:$(date +%F) .
 docker login
-#docker tag 440f15e3e6a3 gondolav/thera-blog:$(date +%F)
 docker push gondolav/thera-blog:$(date +%F)
